bafff99b6910204e8105bef7a62714da2e599626
VladVasikov <92257817+VladVasikov@users.noreply.github.com>
2024-03-15 08:52:24 +0500

    Add files via upload

--- Transfer_data.c
+++ Transfer_data.c
@@ -1,11 +1,8 @@
 #include "Transfer_data.h"
 
 int adr=1;
-
 //struct data buff_data;
-void init(){
 
-}
 void read_data(char *data_, int count_byte, struct data_read* read_buff)
 {
    if (count_byte>0){
@@ -21,30 +18,29 @@ int process_read_data(struct data_read* read_buff, struct data* buff_data){
         if( (read_buff->buff[0]==1) && (read_buff->buff[1]==2) && (read_buff->buff[2]==3) && (read_buff->buff[3]==4) ){
            if(read_buff->buff[4]<=read_buff->count){
                if(read_buff->buff[6]==adr){
-                 // printf("crc %d \n",crc(&read_buff->buff, read_buff->buff[4]-1));
+                  //printf("crc %d \n",crc(&read_buff->buff, read_buff->buff[4]-1));
                   if(crc(&read_buff->buff, read_buff->buff[4]-1)==read_buff->buff[read_buff->buff[4]-1]) {
-                      for(int i=0; i< read_buff->buff[4]-8; i++){
-                        buff_data->buff[i]=read_buff->buff[i+7];
-                      }
+                      for(int i=0; i< read_buff->buff[4]-8; i++){ buff_data->buff[i]=read_buff->buff[i+7];}
+
                       //memcpy(&buff_data->buff, &read_buff->buff[7], read_buff->buff[4]-8);
                       buff_data->adr_source= read_buff->buff[5];
                       buff_data->count =read_buff->buff[4]-8;
-
+                      read_buff->count-=read_buff->buff[4];
+                      read_buff->time=0;
                   }else{ read_buff->count=0;}
                } else{ read_buff->count=0;}
-               read_buff->count-= read_buff->buff[4];
-               memcpy(&read_buff->buff[0], &read_buff->buff[read_buff->buff[4]], read_buff->count);
            }
         }else{
           read_buff->count=0;
         }
-    }else {return 0;}
+    }
     if(read_buff->count>0){
-      read_buff->time++;
       if(read_buff->time>10){
         read_buff->count=0;
+        read_buff->time=0;
       }
     }
+    read_buff->time++;
     return 1;
 }
 
@@ -57,6 +53,4 @@ char crc(char *data, int count){
  return char_var;
 }
 
-void set_adr(int data){
-  adr=data;
-}
+void set_adr(int data){adr=data;}
--- main.c
+++ main.c
@@ -1,29 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "Transfer_data.h"
 char data[150];
-
-
-static struct data_read read_buff;
-static struct data buff_data;
+struct data_read read_buff;
+struct data buff_data;
 
 int main()
 {
-    char name[150];
-    while(1){
-        printf("Input transfer data: ");
-        set_adr(7);
-       // int count_byte= scanf("%[^\n]", data);
-        int count_byte= scanf("%d", data);
-        read_data(data, count_byte, &read_buff);
-        process_read_data(&read_buff, &buff_data);
-        if(buff_data.count>0){
-         printf("data: %d  count: %d  source: %d \n", buff_data.buff[0], buff_data.count, buff_data.adr_source);
+    char name[250];
+    set_adr(7);
+    char * filename = "input.txt";
+    char buffer[256];
+    FILE *fp = fopen(filename, "r");
+    if(fp)
+    {
+        while((fgets(buffer, 256, fp))!=NULL)
+        {
+            int i=0, num;
+            char *p, *p1;
+            char p2[]=" ";
+            p=&buffer[0];
+            i=0;
+            while(p1=strstr(p, p2)){
+               sscanf(p, "%d", &num);
+               name[i] = (char)num;
+               p=p1+1;
+               i++;
+            }
+            read_data(&name[0], i, &read_buff);
+           // for(int i=0; i<read_buff.count; i++){printf("%d ",read_buff.buff[i]);}
+           // printf("  **** time: %d \n",read_buff.time);
+            process_read_data(&read_buff, &buff_data);
+            if(buff_data.count>0){
+               printf("count: %d  source: %d  data: ", buff_data.count, buff_data.adr_source);
+               for(int i=0; i<buff_data.count; i++){printf("%d ",buff_data.buff[i]);}
+            printf("\n");
+               buff_data.count=0;
+            }
         }
-
+        fclose(fp);
     }
-
-
-
     return 0;
 }
